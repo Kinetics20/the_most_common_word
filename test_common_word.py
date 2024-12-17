d0419e287175c1efabf4d68b3da26a5e96d02eaa
Kinetics20 <kinetics20@gmail.com>
2024-12-17 16:23:48 +0100

    add empty text case test for most_common_word fn

--- test_common_word.py
+++ test_common_word.py
@@ -10,3 +10,7 @@ def test_most_common_word_case_insensitivity():
     assert most_common_word(
         "In a small city, life is quieter, but in the City of Angels, everyone dreams big, "
         "and no one can ignore the city’s vibrant energy.") == ('city', 3)
+
+
+def test_most_common_word_empty_text():
+    assert most_common_word("") == ('', 0)
