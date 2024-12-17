869e3fe191bc6e071e7c9f878249e7d4339d8fe1
Kinetics20 <kinetics20@gmail.com>
2024-12-17 16:17:40 +0100

    add case insensitivity test for most_common_word fn

--- test_common_word.py
+++ test_common_word.py
@@ -4,3 +4,9 @@ from common_word import most_common_word
 def test_most_common_word_basic():
     assert most_common_word("Home is where I feel safe, but the house I grew up in will always be home to me.") == (
         'home', 2)
+
+
+def test_most_common_word_case_insensitivity():
+    assert most_common_word(
+        "In a small city, life is quieter, but in the City of Angels, everyone dreams big, "
+        "and no one can ignore the city’s vibrant energy.") == ('city', 3)
